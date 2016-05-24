51a205357bfc1c6fddea193249395794a8d0b0ba
Burak Aslan <liongate2@gmail.com>
2016-05-24 16:56:23 -0400

    added files

---
+++ ps2/Dijkstra.java
@@ -0,0 +1,24 @@
+/**
+
+  Burak Aslan
+  Eli Abidor
+  CS4800
+  PS2
+  Dijkstra.java - reads and writes to files
+
+  2 arguments
+  The first is the file should be a list of edges that make up a graph,
+  one edge per line, in the format:
+
+  vertexname1,vertexname2=distance
+
+  The second is a list of updates to the graph in the format:
+
+  vertexname1,vertexname2=D
+**/
+
+public class Dijkstra {
+  public static void main(String [] args) {
+
+  }
+}
